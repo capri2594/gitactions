d2feab673e129451232dfad4bf43733d059ab433
Reynaldo <capri.2594@gmail.com>
2026-02-11 22:11:05 -0400

    agregando ejercicio 2

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 // Crea test.js
 const { sum } = require('./app');
+
 console.log('Running tests...');
+
 if (sum(2, 3) === 5) {
-console.log(' Test passed');
-process.exit(0);
+    console.log(' Test passed');
+    process.exit(0);
 } else {
-console.log(' Test failed');
-process.exit(1);
+    console.log(' Test failed');
+    process.exit(1);
 }
